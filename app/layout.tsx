2302754a9173b832a106befe96505c83a8eef5fc
Muhammad Shah <numl-s22-10226@numls.edu.pk>
2025-07-24 20:36:18 +0500

    Full and Final Working

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,8 @@ import './globals.css'
 
 export const metadata: Metadata = {
   title: 'Lead Better',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  description: 'Created with ❤️',
+  generator: 'MisterShah',
 }
 
 export default function RootLayout({
